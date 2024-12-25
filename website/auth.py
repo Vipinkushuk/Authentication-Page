a548eb00a1de18723952c07ab11288e38091ac0f
vipinkushuk <vipinkhushuk@gmail.com>
2024-12-25 13:41:16 +0530

    Add new project files

--- website/auth.py
+++ website/auth.py
@@ -1,23 +1,37 @@
-from flask import Blueprint,render_template,redirect,url_for,Request
+from flask import Blueprint,render_template,redirect,url_for,request,flash
 auth = Blueprint("auth",__name__)
 
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-
-    email = Request.user.get("email")
-    password = Request.user.get("password")
+    email = request.form.get("email")
+    password1 = request.form.get("password1")
     return render_template("login.html")
     
-@auth.route('/sign_up', methods=['GET','POST'])
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user_name = request.form.get('user_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-    email = Request.user.get("email")
-    username = Request.user.get("username")
-    password1 = Request.user.get("password1")
-    password2 = Request.user.get("password2")
-    return render_template("sign_up.html")
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(user_name) < 2:
+            flash('Username must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created successfully!', category='success')
+            # You can redirect after successful action
+            return redirect(url_for('auth.login'))  # assuming you have a login route
+
+    return render_template('sign_up.html')
+
     
 @auth.route('/logout')
 def logout():
-    return redirect(url_for("views.home"))
+    return redirect(url_for("auth.login"))
